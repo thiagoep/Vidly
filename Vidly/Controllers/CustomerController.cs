8a378479c8e209564fbea6c9ae0ee838117c1e9c
Thiago <thiagoexequielperez@gmail.com>
2023-04-06 19:24:02 -0300

    Ejercicio 1

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Thiago" },
+                new Customer { Id = 2, Name = "Exequiel" }
+            };
+
+        // GET: Customers
+        [Route("customers")]
+        public ActionResult Customers()
+        {
+            return View(customers);
+        }
+
+        [Route("customers/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            Customer customer = customers.Find(x => x.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public List<Movie> movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "SpongeBob" }
+            };
+
+        public List<Customer> customers = new List<Customer>
+            {
+                new Customer { Name = "Thiago" },
+                new Customer { Name = "Exequiel" }
+            };
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+        }
+
+        [Route("movies")]
+        public ActionResult Movies()
+        {
+
+            return View(movies);
+        }
+
+        [Route("movies/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            Movie movie = movies.Find(x => x.Id == id);
+
+            if(movie == null)
+            {
+                return HttpNotFound();
+            } 
+            else
+            {
+                return View(movie);
+            }
+        }
+
+    }
+}
